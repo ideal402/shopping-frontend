9c6d2f2826c03a8cd4da35004b2247b7c0f065ff
ideal402 <sksekdh@gmail.com>
2024-10-30 13:19:29 +0900

    [feat] 쿼리 적용해서 상품 검색

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -7,10 +7,10 @@ export const getProductList = createAsyncThunk(
   "products/getProductList",
   async (query, { rejectWithValue }) => {
     try {
-      const response = await api.get("/product");
-      if(response.status !== 200) throw new Error(response.error);
+      const response = await api.get("/product", { params: { ...query } });
+      if (response.status !== 200) throw new Error(response.error);
 
-      return response.data.data;
+      return response.data;
     } catch (error) {
       rejectWithValue(error.error);
     }
@@ -72,32 +72,33 @@ const productSlice = createSlice({
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(createProduct.pending, (state) => {
-      state.loading = true;
-    })
-    .addCase(createProduct.fulfilled, (state) => {
-      state.loading = false;
-      state.error = "";
-      state.success = true;
-    })
-    .addCase(createProduct.rejected, (state, action) => {
-      state.loading = false;
-      state.error = action.payload;
-      state.success = false;
-    })
-    .addCase(getProductList.pending, (state)=>{
-      state.loading = true;
-    })
-    .addCase(getProductList.fulfilled, (state, action)=>{
-      state.loading = false;
-      state.error = "";
-      state.productList = action.payload;
-    })
-    .addCase(getProductList.rejected, (state, action)=>{
-      state.loading = false
-      state.error = action.payload;
-    })
-    ;
+    builder
+      .addCase(createProduct.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(createProduct.fulfilled, (state) => {
+        state.loading = false;
+        state.error = "";
+        state.success = true;
+      })
+      .addCase(createProduct.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.success = false;
+      })
+      .addCase(getProductList.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getProductList.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = "";
+        state.productList = action.payload.data;
+        state.totalPageNum = action.payload.totalPageNum;
+      })
+      .addCase(getProductList.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
   },
 });
 
