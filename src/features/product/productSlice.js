fa4101824f83f9af33dc01e84e163ed48209d886
ideal402 <sksekdh@gmail.com>
2024-10-30 12:24:01 +0900

    [feat] 상품 정보 가져오기 추가

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -5,7 +5,16 @@ import { showToastMessage } from "../common/uiSlice";
 // 비동기 액션 생성
 export const getProductList = createAsyncThunk(
   "products/getProductList",
-  async (query, { rejectWithValue }) => {}
+  async (query, { rejectWithValue }) => {
+    try {
+      const response = await api.get("/product");
+      if(response.status !== 200) throw new Error(response.error);
+
+      return response.data.data;
+    } catch (error) {
+      rejectWithValue(error.error);
+    }
+  }
 );
 
 export const getProductDetail = createAsyncThunk(
@@ -24,8 +33,6 @@ export const createProduct = createAsyncThunk(
       );
       return response.data.data;
     } catch (error) {
-      console.log("🚀 ~ error:", error)
-      console.log("🚀 ~ erroreee:", error.error)
       return rejectWithValue(error.error);
     }
   }
@@ -66,18 +73,31 @@ const productSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder.addCase(createProduct.pending, (state) => {
-      state.loading=true;
+      state.loading = true;
     })
-    builder.addCase(createProduct.fulfilled, (state) => {
-      state.loading=false;
-      state.error="";
-      state.success=true;
+    .addCase(createProduct.fulfilled, (state) => {
+      state.loading = false;
+      state.error = "";
+      state.success = true;
     })
-    builder.addCase(createProduct.rejected, (state, action) => {
-      state.loading=false;
-      state.error = action.payload
-      state.success=false;
+    .addCase(createProduct.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+      state.success = false;
     })
+    .addCase(getProductList.pending, (state)=>{
+      state.loading = true;
+    })
+    .addCase(getProductList.fulfilled, (state, action)=>{
+      state.loading = false;
+      state.error = "";
+      state.productList = action.payload;
+    })
+    .addCase(getProductList.rejected, (state, action)=>{
+      state.loading = false
+      state.error = action.payload;
+    })
+    ;
   },
 });
 
