0495d3bb455bd131402942fdcd4f289617dd39d5
ideal402 <sksekdh@gmail.com>
2024-11-04 02:33:38 +0900

    [fix] 로딩스피너 수정

--- src/page/CartPage/CartPage.js
+++ src/page/CartPage/CartPage.js
@@ -12,32 +12,29 @@ const CartPage = () => {
   const { cartList, totalPrice, loading } = useSelector((state) => state.cart);
 
   useEffect(() => {
-    dispatch(getCartList());
+    dispatch(getCartList())
   }, []);
 
   return (
     <Container>
-      {loading ? (
-        <LoadingSpinner />
-      ) : (
-        <Row>
-          <Col xs={12} md={7}>
-            {cartList.length > 0 ? (
-              cartList.map((item) => (
-                <CartProductCard item={item} key={item._id} />
-              ))
-            ) : (
-              <div className="text-align-center empty-bag">
-                <h2>카트가 비어있습니다.</h2>
-                <div>상품을 담아주세요!</div>
-              </div>
-            )}
-          </Col>
-          <Col xs={12} md={5}>
-            <OrderReceipt cartList={cartList} totalPrice={totalPrice} />
-          </Col>
-        </Row>
-      )}
+      {loading?(<LoadingSpinner/>):(<></>)}
+      <Row>
+        <Col xs={12} md={7}>
+          {cartList.length > 0 ? (
+            cartList.map((item) => (
+              <CartProductCard item={item} key={item._id} />
+            ))
+          ) : (
+            <div className="text-align-center empty-bag">
+              <h2>카트가 비어있습니다.</h2>
+              <div>상품을 담아주세요!</div>
+            </div>
+          )}
+        </Col>
+        <Col xs={12} md={5}>
+          <OrderReceipt cartList={cartList} totalPrice={totalPrice}/>
+        </Col>
+      </Row>
     </Container>
   );
 };
