7cd36cf5d9b73f646ebf6c2ab8b960e71796a53c
ideal402 <sksekdh@gmail.com>
2024-11-04 02:33:25 +0900

    [feat] 카트페이지 로딩스피너 업데이트

--- src/page/CartPage/CartPage.js
+++ src/page/CartPage/CartPage.js
@@ -6,34 +6,38 @@ import CartProductCard from "./component/CartProductCard";
 import OrderReceipt from "../PaymentPage/component/OrderReceipt";
 import "./style/cart.style.css";
 import { getCartList } from "../../features/cart/cartSlice";
-
+import LoadingSpinner from "../../common/component/LoadingSpinner";
 const CartPage = () => {
   const dispatch = useDispatch();
-  const { cartList, totalPrice } = useSelector((state) => state.cart);
+  const { cartList, totalPrice, loading } = useSelector((state) => state.cart);
 
   useEffect(() => {
-    dispatch(getCartList())
+    dispatch(getCartList());
   }, []);
 
   return (
     <Container>
-      <Row>
-        <Col xs={12} md={7}>
-          {cartList.length > 0 ? (
-            cartList.map((item) => (
-              <CartProductCard item={item} key={item._id} />
-            ))
-          ) : (
-            <div className="text-align-center empty-bag">
-              <h2>카트가 비어있습니다.</h2>
-              <div>상품을 담아주세요!</div>
-            </div>
-          )}
-        </Col>
-        <Col xs={12} md={5}>
-          <OrderReceipt cartList={cartList} totalPrice={totalPrice}/>
-        </Col>
-      </Row>
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <Row>
+          <Col xs={12} md={7}>
+            {cartList.length > 0 ? (
+              cartList.map((item) => (
+                <CartProductCard item={item} key={item._id} />
+              ))
+            ) : (
+              <div className="text-align-center empty-bag">
+                <h2>카트가 비어있습니다.</h2>
+                <div>상품을 담아주세요!</div>
+              </div>
+            )}
+          </Col>
+          <Col xs={12} md={5}>
+            <OrderReceipt cartList={cartList} totalPrice={totalPrice} />
+          </Col>
+        </Row>
+      )}
     </Container>
   );
 };
